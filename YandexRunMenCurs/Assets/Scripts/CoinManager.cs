63e01c0fd46d6603e8ca7c80eca8516d72806e48
Rgrach <32617088+RomaGrach@users.noreply.github.com>
2023-01-28 19:12:05 +0300

    UI: создание пользовательского интерфейса

--- YandexRunMenCurs/Assets/Scripts/CoinManager.cs
+++ YandexRunMenCurs/Assets/Scripts/CoinManager.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CoinManager : MonoBehaviour
 {
     [SerializeField] private int _NumberOfCoinsInLevel;
+    [SerializeField] TextMeshProUGUI _text;
     
     public void AddOne(){
         _NumberOfCoinsInLevel += 1;
+        _text.text = _NumberOfCoinsInLevel.ToString();
     }
 }
---
+++ YandexRunMenCurs/Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _startMenu;
+
+    // Update is called once per frame
+    public void Play()
+    {
+        _startMenu.SetActive(false);
+    }
+}
